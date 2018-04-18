8eb9c9ffe47ddc935de87259612e31a96c8b243b
gabor <GaborZay@gmail.com>
2018-04-17 22:11:45 -0700

    Brought in firebase for authentication.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from "./components/common";
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAV7PQb_2FSDha6wrgn2KtylPLi999DHmc",
+            authDomain: "auth-fbc31.firebaseapp.com",
+            databaseURL: "https://auth-fbc31.firebaseio.com",
+            projectId: "auth-fbc31",
+            storageBucket: "auth-fbc31.appspot.com",
+            messagingSenderId: "108474473101"
+        })
+    }
+
     render () {
         return (
             <View>
